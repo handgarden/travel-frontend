a088a456549f13b94efa3b6003bb67ef482e62e6
sjungwon <sjungwon03@gmail.com>
2023-08-14 13:37:10 +0900

    refactor: 회원가입 검증 예외 메세지

--- src/lib/func/message.ts
+++ src/lib/func/message.ts
@@ -1,4 +1,5 @@
 export const DUPLICATE_MESSAGE = (target: string) =>
   `중복된 ${target}이 존재합니다.`;
 
-export const SERVER_ERROR_MESSAGE = "서버에 문제가 발생했습니다.";
+export const SERVER_ERROR_MESSAGE =
+  "서버에 문제가 있습니다. 나중에 다시 시도해주세요.";
